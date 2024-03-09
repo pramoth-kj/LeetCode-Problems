2f425515d67faf234a86dd6cf4d6b64501514e82
Pramoth K J <108267585+pramoth-kj@users.noreply.github.com>
2024-03-09 22:42:43 +0530

    Time: 101 ms (12.9%), Space: 13.1 MB (12.9%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.c
+++ 2540-minimum-common-value/2540-minimum-common-value.c
@@ -1,20 +1,27 @@
-int getCommon(int* nums1, int nums1Size, int* nums2, int nums2Size){
-    int b = -1;
-    for(int i = 0; i < nums1Size;i++){
-        int a = nums1[i];
-        int s = 0, e = nums2Size - 1, mid = s + (e-s)/2;
-        while(s<=e){
-            if(nums2[mid] == a){
-                return a;
+int compare(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+
+int getCommon(int *nums1, int nums1Size, int *nums2, int nums2Size) {
+    qsort(nums1, nums1Size, sizeof(int), compare);
+    qsort(nums2, nums2Size, sizeof(int), compare);
+
+    int minCommon = -1;
+    int i = 0, j = 0;
+
+    while (i < nums1Size && j < nums2Size) {
+        if (nums1[i] == nums2[j]) {
+            if (minCommon == -1 || nums1[i] < minCommon) {
+                minCommon = nums1[i];
             }
-            else if(nums2[mid] < a){
-                s = mid +1;
-            }
-            else{
-                e = mid-1;
-            }
-            mid = s + (e-s)/2;
+            i++;
+            j++;
+        } else if (nums1[i] < nums2[j]) {
+            i++;
+        } else {
+            j++;
         }
     }
-    return b;
+
+    return minCommon;
 }
